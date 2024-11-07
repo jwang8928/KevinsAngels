06cdc6c159ad4bc2683470877d3c2775e9714faf
klsm245 <kalistasmiley@Kalistas-MacBook-Pro.local>
2024-11-07 17:07:19 -0500

    Updated the task feature - now includes slider for amount

--- CS498-Prototype/Assets/UIDemo.cs
+++ CS498-Prototype/Assets/UIDemo.cs
@@ -1,12 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIDemo : MonoBehaviour
 {
-    public TextMeshProUGUI output;
-    public TMP_InputField username;
+
+    // Entering New Task Variables
+    public TMP_InputField inputNewTask;
+
+
+    // Assinging Amount to New Task
+    public Slider newTaskAmount;
+    public TextMeshProUGUI newTaskAmountTxt;
+    
+    
+    public TextMeshProUGUI listNewTaskTxt;
+    public TextMeshProUGUI listNewTaskAmountTxt;
+
+
+    private void Start() {
+        inputNewTask.onSubmit.AddListener(delegate { ButtonDemo(); });
+        
+        UpdateSliderValue(newTaskAmount.value);
+
+        newTaskAmount.onValueChanged.AddListener(UpdateSliderValue);
+    }
+
+    private void UpdateSliderValue(float value) {
+        int intSliderValue = Mathf.RoundToInt(value);
+        newTaskAmountTxt.text = intSliderValue.ToString();
+    }
 
     public void ButtonDemo() {
-        output.text = username.text;
+        listNewTaskTxt.text = inputNewTask.text;
+        listNewTaskAmountTxt.text = newTaskAmountTxt.text;
+
+        // Reset Input fields
+        inputNewTask.text = "";
+        newTaskAmount.value = 0;
     }
 }
