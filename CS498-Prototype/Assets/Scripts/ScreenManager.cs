e465d2aeeb5e9cdf5aded8683adac5ac769d1858
emoriej <eja249@uky.edu>
2024-11-08 01:31:13 -0500

    added market

--- CS498-Prototype/Assets/Scripts/ScreenManager.cs
+++ CS498-Prototype/Assets/Scripts/ScreenManager.cs
@@ -8,4 +8,13 @@ public class ScreenManager : MonoBehaviour
         SceneManager.LoadScene("SampleScene");
     }
     
+    public void GoBack()
+    {
+        SceneManager.LoadScene("UI");
+    }
+
+    public void GoToMarket()
+    {
+        SceneManager.LoadScene("Market");
+    }
 }
